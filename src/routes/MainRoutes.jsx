7b043e02630ec047b1d5ab13f663b44d46d073f5
bekamax <bekamax05@gmail.com>
2024-05-03 19:03:08 +0600

    main

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/home/Home";
+import Registor from "../pages/registor/Registor";
+import Login from "../pages/logIn/Login";
 
 const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/registor" element={<Registor />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
