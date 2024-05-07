eed8297b7ae270219d4223c30e48eae0b4843fc2
Saintfather08 <157385663+Saintfather08@users.noreply.github.com>
2024-05-07 18:13:02 +0600

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Navbar from "./widgets/navbar/navbar";
 import MainRoutes from "./routes/MainRoutes";
+import Footer from "./pages/footer/FooterPage";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <MainRoutes />
-      
+      {/* <Footer /> */}
     </div>
   );
 };
